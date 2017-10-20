2d4f206b58c63611fce7df47cc01c1d2cc92048b
Matheus Lessa Rodrigues <matheuslessarodrigues@gmail.com>
2017-10-20 14:49:50 -0200

    agora sim ta pronto pra zua tudo

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public sealed class Bullet : MonoBehaviour
+{
+    public Rigidbody2D bulletRigidbody;
+
+    public Vector2 impulse;
+
+    private void Start()
+    {
+        bulletRigidbody.AddForce(impulse, ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Cannon.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public sealed class Cannon : MonoBehaviour
+{
+    public Bullet bulletPrefab;
+    public float instantiateOffset = 10.0f;
+    public float shootImpulse = 30.0f;
+
+    public Vector3 startingDirection = Vector3.left;
+
+    private void Update()
+    {
+        Vector3 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 mouseDirection = Input.mousePosition - screenPosition;
+
+        var rotation = Quaternion.FromToRotation(startingDirection, mouseDirection);
+        transform.localRotation = rotation;
+
+        if( Input.GetMouseButtonDown(0))
+        {
+            Vector2 direction = ((Vector2)mouseDirection).normalized;
+            Vector3 position = transform.position + (Vector3) direction * instantiateOffset;
+
+            var bullet = Instantiate(bulletPrefab.gameObject, position, Quaternion.identity).GetComponent<Bullet>();
+            bullet.impulse = direction * shootImpulse;
+        }
+    }
+}
